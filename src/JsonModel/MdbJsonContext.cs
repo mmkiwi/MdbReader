fcaa2f0968ed1b98e632eaca868e5b6f8ae2a164
Micah Makaiwi <micah@mmkiwi.com>
2022-12-26 09:40:39 -0500

    Adding missing MPL headers

--- src/JsonModel/MdbJsonContext.cs
+++ src/JsonModel/MdbJsonContext.cs
@@ -1,4 +1,10 @@
-﻿using System.Text.Json.Serialization;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Based on code from libmdb (https://github.com/mdbtools/mdbtools)
+
+using System.Text.Json.Serialization;
 
 namespace MMKiwi.MdbReader.JsonModel;
 
